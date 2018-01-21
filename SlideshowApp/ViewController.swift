1b9e0e03af1082b892f769b612bc2852a03a7630
高木耕平 <takapoko182@gmail.com>
2018-01-21 09:52:09 +0900

    2018年1月21日日曜日午前１０時ごろに修正した

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,20 +13,32 @@ class ViewController: UIViewController {
     //１画面目の写真のアウトレット設定
     @IBOutlet weak var FirstPicture: UIImageView!
     
+    //進む・戻る・再生停止ボタン
+    
+    @IBOutlet weak var go: UIButton!
+    @IBOutlet weak var back: UIButton!
+    @IBOutlet weak var slide: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
+    //最初の写真を表示する
+        let FirstPicture = UIImage(named: "IMG_0396.jpg")
+        
+        
+        
+        
+        
+        
+        
     //写真をタップしたら遷移するコード
-        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapped(_:)))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(FirstPicture.tap(_:)))
         
         self.view.addGestureRecognizer(tap)
         
-        @objc func tapped(_ sender:UITapGestureRecognizer){
-            
-            
-        }
+        
         
     
     
